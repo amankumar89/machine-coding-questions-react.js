55f6d3b6075c88393aa6822c8cb784843b63a134
amankumar89 <amankumaroo784@gmail.com>
2026-03-01 11:34:04 +0530

    Refactor Layout component to enhance navigation and styling. Added a back button for improved user experience and adjusted layout structure for better responsiveness.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,25 @@
+import { useNavigate } from "react-router";
+import { MoveLeftIcon } from "lucide-react";
+
 type LayoutProps = {
     title: string;
     children: React.ReactNode;
 }
 const Layout = ({ title, children }: LayoutProps) => {
+    const navigate = useNavigate();
+
     return (
-        <div className="w-full min-h-screen p-10">
-            <h1 className="text-3xl font-bold text-center mb-10">
-                {title ?? 'Machine Coding Question'}
-            </h1>
+        <div className="w-full h-screen py-8">
+            <div className="flex items-center justify-center mb-8 px-8">
+                {window && window.location.pathname !== '/' && (
+                    <button type="button" onClick={() => navigate('/')} className="mr-8 border rounded-sm px-2">
+                        <MoveLeftIcon />
+                    </button>
+                )}
+                <span className="text-3xl font-bold text-center">
+                    {title ?? 'Machine Coding Question'}
+                </span>
+            </div>
             {children}
         </div>
     );
