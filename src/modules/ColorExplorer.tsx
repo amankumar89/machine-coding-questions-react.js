be51bba7178390efb71d1e01aee161ad7a7513e3
amankumar89 <amankumaroo784@gmail.com>
2026-03-01 10:10:19 +0530

    Add color utility functions and ColorExplorer component for color name to hex conversion

--- src/modules/ColorExplorer.tsx
+++ src/modules/ColorExplorer.tsx
@@ -1,9 +1,65 @@
+import { Search } from "lucide-react"
 import Layout from "../components/Layout"
+import { useRef, useState } from "react"
+import { colorNameToHex } from "../helper";
 
 const ColorExplorer = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const [colorData, setColorData] = useState({
+        name: '',
+        hex: '',
+    });
+
+    function handleClick() {
+        const colorName = inputRef.current!.value!;
+        const hex = colorNameToHex(colorName);
+
+        setColorData((prev) => ({
+            ...prev,
+            name: colorName,
+            hex,
+        }))
+    }
+
     return (
         <Layout title="Color Explorer">
-            <div>Color Explorer App</div>
+            {/* container */}
+            <div className="w-1/2 mx-auto my-0">
+                {/* top container */}
+                <div className="flex items-center w-full mx-auto bg-white border border-gray-300 rounded-md shadow-sm focus-within:ring-2 focus-within:ring-blue-500 focus-within:border-blue-500 transition">
+
+                    <input
+                        ref={inputRef}
+                        type="text"
+                        placeholder="Search..."
+                        className="flex-1 px-4 py-2 bg-transparent outline-none text-gray-700 placeholder-gray-400"
+                    />
+
+                    <button
+                        onClick={handleClick}
+                        className="px-4 py-2 text-gray-500 hover:text-blue-600 transition-colors duration-200"
+                    >
+                        <Search className="w-5 h-5" />
+                    </button>
+
+                </div>
+                {/* bottom container */}
+                {colorData.name ? (
+                    <div className="grid place-items-center bg-white rounded-md my-4 py-8">
+                        <div className="w-36 h-36 rounded-full" style={{
+                            background: colorData?.hex
+                        }} />
+                        <div className="my-4">
+                            <strong>Name:</strong><span>{` ${colorData.name}`}</span>
+                        </div>
+                        <div>
+                            <strong>Hex:</strong><span>{` ${colorData.hex}`}</span>
+                        </div>
+                    </div>
+                ) : null}
+
+            </div>
         </Layout>
     )
 }
